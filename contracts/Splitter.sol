70c81e503a6d9d88ccc7e3fc48f80aab7438aa1a
naveen <172697+naveensrinivasan@users.noreply.github.com>
2018-04-27 22:29:05 -0400

    Initial Commit.

---
+++ contracts/Splitter.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.23;
+contract Splitter is Destructible{
+    using SafeMath for uint256;
+
+    uint256 public constant TOTAL = 100;
+    uint256 public totalWithdrawn = 0;
+
+    mapping(address => uint256) public percentOwned;
+    mapping(address => uint256) public released;
+    address[] public receivers;
+
+    event AmountWithDrawn(address receiver, uint256 amount);    
+    constructor (address _receiver1, address _receiver2) public payable {
+        addReciever(_receiver1, 50);
+        addReciever(_receiver2, 50);
+    }
+
+    function () public payable {}
+
+    function withdraw() public {
+      address receiver = msg.sender;
+
+      uint256 totalReceived = address(this).balance.add(totalWithdrawn);
+      uint256 payment=totalReceived.mul(percentOwned[receiver]).div(TOTAL).sub(released[receiver]);
+
+      require(payment != 0);
+      require(address(this).balance >= payment);
+
+      released[receiver] = released[receiver].add(payment);
+      totalWithdrawn = totalWithdrawn.add(payment);
+
+      receiver.transfer(payment);
+
+      emit AmountWithDrawn(receiver, payment);
+    }
+
+    function addReciever(address _reciever, uint256 _percent) internal {
+      require(_reciever != address(0));
+      require(_percent > 0);
+      require(percentOwned[_reciever] == 0);
+
+      receivers.push(_reciever);
+      percentOwned[_reciever] = _percent;
+    }
+} 
